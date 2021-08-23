2e0b291482a598183529e5cb3090f34ad20b8487
Gilbert Subay <gilbertttsubay@gmail.com>
2021-08-23 07:37:00 +0700

    setup spring project

---
+++ src/main/java/com/example/latihangit/LatihanGitApplication.java
@@ -0,0 +1,13 @@
+package com.example.latihangit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LatihanGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LatihanGitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/latihangit/LatihanGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.latihangit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LatihanGitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
